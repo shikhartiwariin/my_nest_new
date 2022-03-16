d4c57ac1cd3339b3db8afa3f444ed87c46971e39
shikhartiwariin <shikhertiwari@gmail.com>
2022-03-16 07:39:55 +0530

    march16_first Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_nest/resident_registration.dart';
 import 'package:my_nest/visitor_management/visitor_form.dart';
 import 'package:my_nest/visitor_management/visitor_summary.dart';
 
