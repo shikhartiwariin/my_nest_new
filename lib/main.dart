ae2b4287bb2ac8486c67ffc19abb3f91458ed679
shikhartiwariin <shikhertiwari@gmail.com>
2022-03-20 10:34:01 +0530

    March20 Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 // import 'package:my_nest/resident_management/resident_registration.dart';
-// import 'package:my_nest/staff_management/staff_attendence.dart';
+import 'package:my_nest/staff_management/staff_attendence.dart';
 // import 'package:my_nest/staff_management/staff_registration.dart';
-import 'package:my_nest/staff_management/staff_summary.dart';
+// import 'package:my_nest/staff_management/staff_summary.dart';
+// import 'package:my_nest/visitor_management/visitor_form.dart';
+// import 'package:my_nest/visitor_management/visitor_summary.dart';
 
 void main() {
-  runApp(const StaffSummary());
+  runApp(const StaffAttendence());
 }
--- lib/staff_management/staff_attendence.dart
+++ lib/staff_management/staff_attendence.dart
@@ -14,89 +14,90 @@ class StaffAttendence extends StatelessWidget {
       title: _title,
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
+        body: const AttendenceSheet(),
       ),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
+class AttendenceSheet extends StatefulWidget {
+  const AttendenceSheet({Key? key}) : super(key: key);
 
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  State<AttendenceSheet> createState() => _AttendenceSheetState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+class _AttendenceSheetState extends State<AttendenceSheet> {
   static const int numItems = 20;
 
   List<bool> selected = List<bool>.generate(numItems, (int index) => false);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 150),
+    return Center(
       child: SizedBox(
-        width: 1000,
-        child: DataTable(
-          columns: const <DataColumn>[
-            DataColumn(
-              label: Text('Present / Absent'),
-            ),
-            DataColumn(
-              label: Text('Staff ID'),
-            ),
-            DataColumn(
-              label: Text('Staff Name'),
-            ),
-            DataColumn(
-              label: Text('In Time'),
-            ),
-            DataColumn(
-              label: Text('Out Time'),
-            ),
-          ],
-          rows: List<DataRow>.generate(
-            numItems,
-            (int index) => DataRow(
-              color: MaterialStateProperty.resolveWith<Color?>(
-                  (Set<MaterialState> states) {
-                // All rows will have the same selected color.
-                if (states.contains(MaterialState.selected)) {
-                  return Theme.of(context)
-                      .colorScheme
-                      .primary
-                      .withOpacity(0.08);
-                }
-                // Even rows will have a grey color.
-                if (index.isEven) {
-                  return Colors.white.withOpacity(0.0);
-                }
-                return null; // Use default value for other states and odd rows.
-              }),
-              cells: <DataCell>[
-                DataCell(
-                  Text('Absent $index'),
-                ),
-                DataCell(
-                  Text('Staff ID $index'),
-                ),
-                DataCell(
-                  Text('Staff Name $index'),
-                ),
-                DataCell(
-                  Text('In Time $index'),
-                ),
-                DataCell(
-                  Text('Out Time $index'),
-                ),
-              ],
-              selected: selected[index],
-              onSelectChanged: (bool? value) {
-                setState(() {
-                  selected[index] = value!;
-                });
-              },
+        width: 1200,
+        child: SingleChildScrollView(
+          child: DataTable(
+            columns: const <DataColumn>[
+              DataColumn(
+                label: Text('Present / Absent'),
+              ),
+              DataColumn(
+                label: Text('Staff ID'),
+              ),
+              DataColumn(
+                label: Text('Staff Name'),
+              ),
+              DataColumn(
+                label: Text('In Time'),
+              ),
+              DataColumn(
+                label: Text('Out Time'),
+              ),
+            ],
+            rows: List<DataRow>.generate(
+              numItems,
+              (int index) => DataRow(
+                color: MaterialStateProperty.resolveWith<Color?>(
+                    (Set<MaterialState> states) {
+                  // All rows will have the same selected color.
+                  if (states.contains(MaterialState.selected)) {
+                    return Theme.of(context)
+                        .colorScheme
+                        .primary
+                        .withOpacity(0.08);
+                  }
+                  // Even rows will have a grey color.
+                  if (index.isEven) {
+                    return Colors.white.withOpacity(0.0);
+                  }
+                  return null; // Use default value for other states and odd rows.
+                }),
+                cells: <DataCell>[
+                  DataCell(
+                    Text('P/A'),
+                  ),
+                  DataCell(
+                    Text('Staff ID $index'),
+                  ),
+                  DataCell(
+                    Text('Staff Name $index'),
+                  ),
+                  DataCell(
+                    Text('In Time $index'),
+                  ),
+                  DataCell(
+                    Text('Out Time $index'),
+                  ),
+                ],
+                selected: selected[index],
+                onSelectChanged: (bool? value) {
+                  setState(() {
+                    selected[index] = value!;
+                  });
+                },
+              ),
             ),
           ),
         ),
